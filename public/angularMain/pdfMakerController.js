df666a347fce653ba8d8f2a9a56defbe8bc3c92f
mykhie <osoromichael@gmail.com>
2017-03-18 17:40:48 +0300

    added excel button participatory report

--- public/angularMain/pdfMakerController.js
+++ public/angularMain/pdfMakerController.js
@@ -39,12 +39,19 @@ pdfModule.controller('PdfController', function ($scope, EptServices) {
                 //    style: ['centerData'],
                 //    margin: [0, 0, 0, 5]
                 //},
-                {text: reportHeader.toUpperCase(), style: ['header', 'centerData'], margin: [0, 0, 0, 5]},
-                {text: 'Nation Public Health Laboratories', style: ['content', 'centerData']},
+                {text: 'Nation Public Health Laboratories'.toUpperCase(), style: ['header', 'centerData']},
+                {text: reportHeader, style: ['content', 'centerData'], margin: [0, 0, 0, 5]},
+
                 {text: 'P.O BOX 145-00100,Nairobi', style: ['content', 'centerData']},
                 {text: 'info@nphl.co.ke', style: ['content', 'centerData']},
-                {text: 'www.nphl.or.ke',link:"http://www.nphl.or.ke", style: ['content', 'centerData'],fillColor: 'yellow', decoration:"underline"},
-                 {text: reportTitle, style: ['subHeader', 'centerData'], margin: [0, 5, 0, 0]},
+                {
+                    text: 'www.nphl.or.ke',
+                    link: "http://www.nphl.or.ke",
+                    style: ['content', 'centerData'],
+                    fillColor: 'yellow',
+                    decoration: "underline"
+                },
+                {text: reportTitle, style: ['subHeader', 'centerData'], margin: [0, 5, 0, 0]},
                 // {text:"Google", link:"http://www.google.com", decoration:"underline",fillColor: 'blue', margin: [0, 5, 0, 0]},
                 {
                     text: "_______________________________________________________________________________________________",
@@ -69,8 +76,12 @@ pdfModule.controller('PdfController', function ($scope, EptServices) {
                         vLineColor: function (i, node) {
                             return (i === 0 || i === node.table.widths.length) ? 'black' : 'black';
                         },
-                        // paddingLeft: function(i, node) { return 4; },
-                        // paddingRight: function(i, node) { return 4; },
+                        paddingLeft: function (i, node) {
+                            return 1;
+                        },
+                        paddingRight: function (i, node) {
+                            return 1;
+                        }
                         // paddingTop: function(i, node) { return 2; },
                         // paddingBottom: function(i, node) { return 2; }
                     }
@@ -81,7 +92,7 @@ pdfModule.controller('PdfController', function ($scope, EptServices) {
                 return {
                     columns: [
                         {
-                            text:'ABNO Softwares International',
+                            text: 'ABNO Softwares International',
                             style: ['content']
                         },
                         {
@@ -145,4 +156,196 @@ pdfModule.controller('PdfController', function ($scope, EptServices) {
             EptServices.EptServiceObject.returnNoRecordsFoundAlert();
         }
     }
+
+
+    $scope.pdfMake.generateShipmentPdf = function (data) {
+        var reportData = new Array();
+        var tableWidth = ['auto', '*', '*', '*', '*', '*', 'auto']
+        var reportHeader =
+            [
+                {text: ' # ', style: 'subHeader'},
+                {text: 'Lab Name', style: 'subHeader'},
+                {text: 'Sample Name', style: 'subHeader'},
+                {text: 'Round Name', style: 'subHeader'},
+                {text: 'Total Sent', style: 'subHeader'},
+                {text: 'Total Rejected', style: 'subHeader'},
+                {text: 'Received', style: 'subHeader'}
+
+            ];
+
+        reportData.push(reportHeader);
+        console.log(data)
+        if (data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+                var lab = data[i];
+                var rowData = [
+                    {text: '' + (i + 1), style: ['content']},
+                    {text: ' ' + lab.lab.institute_name, style: ['content']},
+                    {text: ' ' + lab.sample.batchName, style: ['content']},
+                    {text: ' ' + lab.round.roundName, style: ['content']},
+                    {text: ' ' + lab.totalSent, style: ['content']},
+                    {text: ' ' + lab.rejected, style: ['content']},
+                    {text: ' ' + lab.received, style: ['content']}
+                ];
+
+                reportData.push(rowData);
+            }
+
+
+            var reportSubHeader = 'SHIPMENT RECEIVING REPORT';
+            var reportTitle = 'NPHL Microbiology Reporting';
+            $scope.pdfMake.mainGeneratorFunction(reportSubHeader, reportData, tableWidth, reportTitle);
+        } else {
+            EptServices.EptServiceObject.returnNoRecordsFoundAlert();
+        }
+    }
+
+
+    $scope.pdfMake.generateParticipatoryReportPdf = function (data) {
+        var reportData = new Array();
+        var tableWidth = ['auto', '*', '*', '*', '*', '*', 'auto','*','*']
+        var reportHeader =
+            [
+                {text: ' # ', style: 'subHeader'},
+                {text: 'Sample Name', style: 'subHeader'},
+                {text: 'Round Name', style: 'subHeader'},
+                {text: 'Enrolled Labs', style: 'subHeader'},
+                {text: ' Unresponded', style: 'subHeader'},
+                {text: ' Responded', style: 'subHeader'},
+                {text: ' Evaluated', style: 'subHeader'},
+                {text: ' Unevaluated', style: 'subHeader'},
+                {text: 'Response %', style: 'subHeader'}
+
+            ];
+
+        reportData.push(reportHeader);
+        console.log(data)
+        if (data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+                var sample = data[i];
+                var rowData = [
+                    {text: '' + (i + 1), style: ['content']},
+                    {text: ' ' + sample.sample.batchName, style: ['content']},
+                    {text: ' ' + sample.round.roundName, style: ['content']},
+                    {text: ' ' + sample.totalLabsAndSamples, style: ['content']},
+                    {text: ' ' + sample.totalUnresponded, style: ['content']},
+                    {text: ' ' + sample.totalResponded, style: ['content']},
+                    {text: ' ' + sample.totalTotalEvaluated, style: ['content']},
+                    {text: ' ' + sample.totalTotalUnevaluated, style: ['content']},
+                    {text: ' ' + sample.responseRate, style: ['content']}
+
+                ];
+
+                reportData.push(rowData);
+            }
+
+
+            var reportSubHeader = 'PARTICIPATORY REPORT';
+            var reportTitle = 'NPHL Microbiology Reporting';
+            $scope.pdfMake.mainGeneratorFunction(reportSubHeader, reportData, tableWidth, reportTitle);
+        } else {
+            EptServices.EptServiceObject.returnNoRecordsFoundAlert();
+        }
+    }
+
+
+    $scope.pdfMake.generateLabPerformancePdf = function (data) {
+        var reportData = new Array();
+        var tableWidth = ['auto', '*', '*', 'auto', '*', '*', 'auto', '*', '*', 'auto']
+        var reportHeader =
+            [
+                {text: ' # ', style: 'subHeader'},
+                {text: 'Lab Name', style: 'subHeader'},
+                {text: 'County', style: 'subHeader'},
+                {text: 'Sample', style: 'subHeader'},
+                {text: 'Round', style: 'subHeader'},
+                {text: 'Micro', style: 'subHeader'},
+                {text: 'Micro Agents', style: 'subHeader'},
+                {text: 'Remarks', style: 'subHeader'},
+                {text: 'Grade', style: 'subHeader'},
+                {text: 'Total', style: 'subHeader'}
+
+            ];
+
+        reportData.push(reportHeader);
+        console.log(data)
+        if (data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+                var lab = data[i];
+                var rowData = [
+                    {text: '' + (i + 1), style: ['content']},
+                    {text: ' ' + lab.labName, style: ['content']},
+                    {text: ' ' + lab.county, style: ['content']},
+                    {text: ' ' + lab.batchName, style: ['content']},
+                    {text: ' ' + lab.roundCode, style: ['content']},
+                    {text: ' ' + lab.finalScore, style: ['content']},
+                    {text: ' ' + lab.totalMicroAgentsScore, style: ['content']},
+                    {text: ' ' + lab.remarks, style: ['content']},
+                    {text: ' ' + lab.grade, style: ['content']},
+                    {text: ' ' + (Number(lab.finalScore) + Number(lab.totalMicroAgentsScore)), style: ['content']}
+                ];
+
+                reportData.push(rowData);
+            }
+
+
+            var reportSubHeader = 'LABORATORY PERFORMANCE REPORT';
+            var reportTitle = 'NPHL Microbiology Reporting';
+            $scope.pdfMake.mainGeneratorFunction(reportSubHeader, reportData, tableWidth, reportTitle);
+        } else {
+            EptServices.EptServiceObject.returnNoRecordsFoundAlert();
+        }
+    }
+
+    $scope.pdfMake.generateSurveyReportPdf = function (data) {
+        var reportData = new Array();
+        var tableWidth = ['auto', '*', 'auto', 'auto', '*', '*', 'auto', 'auto', 'auto', 'auto', 'auto']
+        var reportHeader =
+            [
+                {text: ' # ', style: 'subHeader'},
+                {text: 'Lab Name', style: 'subHeader'},
+                {text: 'Lab Code', style: 'subHeader'},
+                {text: 'County', style: 'subHeader'},
+                {text: 'Sample', style: 'subHeader'},
+                {text: 'Round', style: 'subHeader'},
+                {text: 'Micro', style: 'subHeader'},
+                {text: 'Agents', style: 'subHeader'},
+                {text: 'Remarks', style: 'subHeader'},
+                {text: 'Grade', style: 'subHeader'},
+                {text: 'Total', style: 'subHeader'}
+
+            ];
+
+        reportData.push(reportHeader);
+        console.log(data)
+        if (data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+                var lab = data[i];
+                var rowData = [
+                    {text: '' + (i + 1), style: ['content']},
+                    {text: ' ' + lab.labName, style: ['content']},
+                    {text: ' ' + lab.unique_identifier, style: ['content']},
+                    {text: ' ' + lab.county, style: ['content']},
+                    {text: ' ' + lab.batchName, style: ['content']},
+                    {text: ' ' + lab.roundCode, style: ['content']},
+                    {text: ' ' + lab.finalScore, style: ['content']},
+                    {text: ' ' + lab.totalMicroAgentsScore, style: ['content']},
+                    {text: ' ' + lab.remarks, style: ['content']},
+                    {text: ' ' + lab.grade, style: ['content']},
+                    {text: ' ' + (Number(lab.finalScore) + Number(lab.totalMicroAgentsScore)), style: ['content']}
+                ];
+
+                reportData.push(rowData);
+            }
+
+
+            var reportSubHeader = 'ROUND PERFORMANCE REPORT';
+            var reportTitle = 'NPHL Microbiology Reporting';
+            $scope.pdfMake.mainGeneratorFunction(reportSubHeader, reportData, tableWidth, reportTitle);
+        } else {
+            EptServices.EptServiceObject.returnNoRecordsFoundAlert();
+        }
+    }
+
+
 })
