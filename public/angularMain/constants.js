65de4d7370ed8075ab000070972b5c1ff8707d73
Osoro Michael <osoromichael@gmail.com>
2017-04-10 16:46:31 +0300

    exclude constants

--- public/angularMain/constants.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "http://localhost:8088/"
-const SERVER_API_URL = {
-    sampleURL: BASE_URL + 'admin/Bacteriologydbci/',
-    bacteriologyURL: BASE_URL + 'admin/Bacteriologydbci/',
-    reportsURL: BASE_URL + 'admin/reports/',
-    repositoryURL: BASE_URL + 'reports/repository/'
-}
