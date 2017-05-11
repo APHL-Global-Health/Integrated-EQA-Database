44fdd7e7150da7eb17ee675aaaf283f31fd26d25
Thomas M. Nyongesa <mapesa@gmail.com>
2017-05-11 17:48:32 +0300

    Lowercase table name in model: Schemes

--- application/models/DbTable/Schemes.php
+++ application/models/DbTable/Schemes.php
@@ -3,7 +3,7 @@
 class Application_Model_DbTable_Schemes extends Zend_Db_Table_Abstract
 {
 
-    protected $_name = 'vl_Scheme';
+    protected $_name = 'vl_scheme';
     
     public function getAllSchemes(){
 		$sql = $this->select();
