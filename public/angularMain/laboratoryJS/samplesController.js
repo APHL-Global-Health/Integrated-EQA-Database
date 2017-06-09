62a4246ffb8fdd56e677c60591f939f799ce1c51
mykhie <osoromichael@gmail.com>
2017-06-09 12:52:41 +0300

    created printed round results

--- public/angularMain/laboratoryJS/samplesController.js
+++ public/angularMain/laboratoryJS/samplesController.js
@@ -2655,8 +2655,41 @@
             // } catch (Exc) {
             //     console.log(Exc)
             // }
-        }
+        };
+        $scope.samples.generatePDfCanvas = function (div) {
+            
+            var form = $("#" + div),
+                    cache_width = form.width(),
+                    a4 = [595.28, 841.89];  // for a4 size paper width and height
+            $('body').scrollTop(0);
+            createPDF();
+
+//create pdf
+            function createPDF() {
+                getCanvas().then(function (canvas) {
+                    var
+                            img = canvas.toDataURL("image/png"),
+                            doc = new jsPDF({
+                                unit: 'px',
+                                format: 'a4'
+                            });
+                    doc.addImage(img, 'JPEG', 20, 20);
+                    doc.save('Full response report.pdf');
+                    form.width(cache_width);
+                });
+            }
+
+// create canvas object
+            function getCanvas() {
+                form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+                return html2canvas(form, {
+                    imageTimeout: 2000,
+                    removeContainer: true
+                });
+            }
+        };
         $scope.samples.printDiv = function (div) {
+
             // $("#demo").clone().printThis(
             //     {
             //         debug: true,        //     * show the iframe for debugging
@@ -2677,7 +2710,8 @@
             console.log('div loaded ' + div);
             $scope.samples.showBarcode = false;
             $("#" + div).print();
-            console.log(div)
+            console.log(div);
+
         }
         //+++++++++++++++++++++++++++++++++++++++++++++++++RETURN RESULTS+++++++++++++++++++++++++++++++++++++++++++++++
         //==============================================================================================================
