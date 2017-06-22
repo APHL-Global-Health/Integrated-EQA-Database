68ffbcf07f03d96a00ca9571499dbe3d50af0f23
mykhie <osoromichael@gmail.com>
2017-06-22 14:58:16 +0300

    removed control selection from the dropdown

---
+++ database/20170622_1439_alter_reference_tables_control_column.sql
@@ -0,0 +1,17 @@
+ALTER TABLE `reference_result_dbs` 
+CHANGE COLUMN `control` `control` INT(11) NULL DEFAULT 0 ;
+
+ALTER TABLE `reference_result_dts` 
+CHANGE COLUMN `control` `control` INT(11) NULL DEFAULT 0 ;
+
+ALTER TABLE `reference_result_eid` 
+CHANGE COLUMN `reference_result` `reference_result` VARCHAR(255) NULL DEFAULT NULL ,
+CHANGE COLUMN `control` `control` INT(11) NULL DEFAULT 0 ;
+
+ALTER TABLE `reference_result_tb` 
+CHANGE COLUMN `control` `control` INT(11) NULL DEFAULT 0 ;
+
+ALTER TABLE `eanalyze_test`.`reference_result_vl` 
+CHANGE COLUMN `control` `control` INT(11) NULL DEFAULT 0 ;
+
+
