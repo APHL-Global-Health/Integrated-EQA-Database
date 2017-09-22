fca5bb68ec58c966b23580f2867cf021495b07a9
osoromichael <mosoro@abnosoftwares.co.ke>
2017-09-22 11:57:56 +0300

    removed test email function

--- application/modules/admin/controllers/LoginController.php
+++ application/modules/admin/controllers/LoginController.php
@@ -83,10 +83,6 @@ class Admin_LoginController extends Zend_Controller_Action {
             }
         }
     }
-    public function emailtestAction(){
-         $common = new Application_Service_Common();
-        $common->sendGeneralEmail('osoromichael@gmail.com', 'test email', 'User');
-        exit;
-    }
+
 
 }
