fdc52d50411617fd127ebe737db9d76ddc79a457
Osoro Michael <mosoro@abnosoftwares.co.ke>
2018-05-08 17:46:24 +0300

    added loading capa report

--- application/modules/reports/controllers/CorrectiveActionsController.php
+++ application/modules/reports/controllers/CorrectiveActionsController.php
@@ -8,10 +8,10 @@ class Reports_CorrectiveActionsController extends Zend_Controller_Action
         /* Initialize action controller here */
         $ajaxContext = $this->_helper->getHelper('AjaxContext');
         $ajaxContext->addActionContext('index', 'html')
-                    ->addActionContext('corrective-actions-export', 'html')
-                    ->addActionContext('corrective-actions-export-pdf', 'html')
-                    ->initContext();
-        $this->_helper->layout()->pageName = 'report'; 
+            ->addActionContext('corrective-actions-export', 'html')
+            ->addActionContext('corrective-actions-export-pdf', 'html')
+            ->initContext();
+        $this->_helper->layout()->pageName = 'report';
     }
 
     public function indexAction()
@@ -19,7 +19,7 @@ class Reports_CorrectiveActionsController extends Zend_Controller_Action
         if ($this->getRequest()->isPost()) {
             $params = $this->_getAllParams();
             $reportService = new Application_Service_Reports();
-            $response=$reportService->getCorrectiveActionReport($params);
+            $response = $reportService->getCorrectiveActionReport($params);
             $this->view->response = $response;
         }
         $scheme = new Application_Service_Schemes();
@@ -28,27 +28,37 @@ class Reports_CorrectiveActionsController extends Zend_Controller_Action
 
     public function correctiveActionsExportAction()
     {
-       $reportService = new Application_Service_Reports();
-        if($this->getRequest()->isPost()){
+        $reportService = new Application_Service_Reports();
+        if ($this->getRequest()->isPost()) {
             $params = $this->_getAllParams();
-            $this->view->exported=$reportService->exportCorrectiveActionsReport($params);
-        }
-    }
-    
-    public function correctiveActionsExportPdfAction()
-    {
-       $reportService = new Application_Service_Reports();
-        if($this->getRequest()->isPost()){
-            $params = $this->_getAllParams();
-            $this->view->header=$reportService->getReportConfigValue('report-header');
-            $this->view->logo=$reportService->getReportConfigValue('logo');
-            $this->view->logoRight=$reportService->getReportConfigValue('logo-right');
-            $this->view->dateRange=$params['dateRange'];
-            $this->view->shipmentName=$params['shipmentName'];
-            $this->view->result=$reportService->exportCorrectiveActionsReportInPdf($params);
+            $this->view->exported = $reportService->exportCorrectiveActionsReport($params);
         }
     }
 
+    public function correctiveActionsExportPdfAction()
+    {
+        $reportService = new Application_Service_Reports();
+        if ($this->getRequest()->isPost()) {
+            $params = $this->_getAllParams();
+            $this->view->header = $reportService->getReportConfigValue('report-header');
+            $this->view->logo = $reportService->getReportConfigValue('logo');
+            $this->view->logoRight = $reportService->getReportConfigValue('logo-right');
+            $this->view->dateRange = $params['dateRange'];
+            $this->view->shipmentName = $params['shipmentName'];
+            $this->view->result = $reportService->exportCorrectiveActionsReportInPdf($params);
+        }
+    }
+
+    public function capaAction()
+    {
+
+
+    }
+    public function getActions(){
+
+
+        ///
+    }
 }
 
 
