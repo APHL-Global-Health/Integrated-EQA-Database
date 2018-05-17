155b1f727bb1a074b8719001e6d675c648850eff
Osoro Michael <mosoro@abnosoftwares.co.ke>
2018-05-17 10:44:11 +0300

    added addinstructions column sql file

---
+++ database/20180515_0914_add_testingInstructions.sql
@@ -0,0 +1,12 @@
+Ensure cross browser compatibility for the whole application
+Fix the result analysis issues - No negative results - Allow for NA (not applicable) scores instead of zeros
+Update reports in line with the NPHL recommendations
+Results entry form should not load with the results entered for another specimen
+On the result entry form, change the Method of disc diffusion field to a drop-down
+Correct typos on the system eg HUQAS to EUCAST
+The days to respond as shown on participant interface should not include weekends
+A copy of email notifications to participants should be sent to the NMRL administrators
+Review mandatory fields for results submission. The Gram Stain field is not necessarily mandatory
+Remove the sample issuing feature from the participant interface
+Remove the link to the administrative login from the landing page
+Send an email link for setting up a password on registration instead of a password.
