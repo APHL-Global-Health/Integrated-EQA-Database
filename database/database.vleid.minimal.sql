9cfb810c10133bb130e8a7076f80bcc3ce5621b2
Thomas Mapesa <mapesa@gmail.com>
2018-08-03 13:15:39 +0300

    db file edit - disable reference checks

--- database/database.vleid.minimal.sql
+++ database/database.vleid.minimal.sql
@@ -1,3 +1,4 @@
+SET FOREIGN_KEY_CHECKS = 0;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contact_us` (
@@ -1132,3 +1133,5 @@ CREATE TABLE `vl_scheme` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 INSERT INTO `vl_scheme` VALUES (1,'Viral Load'),(2,'EID');
+
+SET FOREIGN_KEY_CHECKS = 1
